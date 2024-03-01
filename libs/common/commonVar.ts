78378438a998a9cd5aa81cb8983307cf3fd84a97
tamagoez <87574268+tamagoez@users.noreply.github.com>
2024-03-01 15:41:27 +0000

    [f/mainbase] minimum supabase setup

---
+++ libs/common/commonVar.ts
@@ -0,0 +1,2 @@
+export const DB_URL = process.env.NEXT_PUBLIC_DB_URL!
+export const DB_KEY = process.env.NEXT_PUBLIC_DB_KEY!
---
+++ libs/db/client.ts
@@ -0,0 +1,44 @@
+import { type CookieOptions, createBrowserClient, createServerClient } from '@supabase/ssr'
+import { DB_KEY, DB_URL } from '../common/commonVar'
+import { cookies } from 'next/headers'
+
+export function createDBBrowserClient() {
+  return createBrowserClient(
+    DB_URL!,
+    DB_KEY!
+  )
+}
+
+export function createDBServerClient() {
+  const cookieStore = cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: '', ...options })
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  )
+}
