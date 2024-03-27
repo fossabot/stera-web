7cb3642a2b17d02d4fd13f1dea304c07a154f903
tamagoez <87574268+tamagoez@users.noreply.github.com>
2024-03-27 02:01:24 +0000

    [TEMP] add debugger

--- app/pwa/route.ts
+++ app/pwa/route.ts
@@ -6,5 +6,6 @@ export async function GET(request: NextRequest) {
     const redirectTo = request.nextUrl.clone()
     // ここでリダイレクト先を分岐するように
     redirectTo.pathname = "/"
+    console.log(`[redirectTo] ${redirectTo.href}`)
     return NextResponse.redirect(redirectTo)
 }
